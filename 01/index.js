7eaf98e89aa2aab8aca7486277238166020424db
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-11-26 21:31:25 +0600

    01-ex done. Added a express server to handle GET requests at /hello

--- 01/index.js
+++ 01/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.get('/hello', (req, res) => {
+    return res.send('Hello');
+});
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
