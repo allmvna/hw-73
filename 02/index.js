ef7484e4e4869f886312921e26ab8b695b228266
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-11-26 22:21:39 +0600

    02-ex. Added routes for text encoding and decoding using 'Vigenere' cipher

--- 02/index.js
+++ 02/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const { Cipher, Decipher } = require('caesar-salad').Vigenere;
 const app = express();
 const port = 8000;
+
+const incognito = 'password';
+
+app.get('/encode/:text', (req, res) => {
+    const text = req.params.text;
+    const encoded = Cipher(incognito).crypt(text);
+    return res.send(`${encoded}`);
+});
+
+app.get('/decode/:text', (req, res) => {
+    const text = req.params.text;
+    const decoded = Decipher(incognito).crypt(text);
+    return res.send(`${decoded}`);
+});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
